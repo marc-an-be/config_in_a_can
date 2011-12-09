e70e222dccfb043ecc0156b52f31aaad2e2b1178
marc-an-be <mab@16d.de>
2011-12-09 01:19:22 +0100

    Couldn't sleep without a nicer solution to the missing key problem and the "modify later" issue ;)

--- lib/config_in_a_can.rb
+++ lib/config_in_a_can.rb
@@ -1,17 +1,25 @@
 module Config
   extend self
   @_config = {}
-  @logger = nil
-  def init(logger=nil, &block)
-    @logger = logger
+  @missing_key_proc = proc{}
+
+  def init(&block)
     instance_eval &block
+  end
+
+  def freeze
+    @_config.freeze
+  end
+
+  def on_missing_key(&block)
+    @missing_key_proc = block_given? ? block : proc{}
   end
 
   def method_missing(name, *args, &block)
     name = (name.to_s.end_with?('=','?') ? name.to_s[0..-2] : name).to_sym
-    return @_config[name] = (args.count == 1 ? args.first : args) if args.any? #&& !@_config.key?(name) # want it unmodifiable?
+    return @_config[name] = (args.count == 1 ? args.first : args) if args.any?
     return @_config[name] if @_config.key?(name)
-    @logger.warn("Key '#{name}' is undefined!") if @logger
+    @missing_key_proc.call name
     nil
   end
 end
